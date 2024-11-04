fd7991ca76b6e012cdaca83ce860d410e6da346f
Eduarda Virgilio <eduarda.virgilio2630@gmail.com>
2024-11-04 15:05:10 -0300

    add message box

--- Controller/UsuarioController.cs
+++ Controller/UsuarioController.cs
@@ -12,41 +12,51 @@ namespace ProjetoAgenda.Controller
     {
         public bool AddUsuario(string nome, string usuario, string senha)
         {
-            //conecta no banco de dados
-            MySqlConnection conexao = ConexaoDB.CriarConexao();
-
-            //oq ele vai executar do sql
-            string sql = "INSERT INTO tbusuarios (nome, usuario, senha) VALUES (@nome, @usuario, @senha);";
-
-            //abri a conexao com o banco de dados
-            conexao.Open();
-
-            //executa o comando sql
-            MySqlCommand comando = new MySqlCommand(sql, conexao);
-
-            //troca o valor dos @ pelas informações que serão cadastradas 
-            //essas informações vieram das funções
-            comando.Parameters.AddWithValue("@nome", nome);
-            comando.Parameters.AddWithValue("@usuario", usuario);
-            comando.Parameters.AddWithValue("@senha", senha);
-
-            //executando no banco de dados
-            int linhasAfetadas = comando.ExecuteNonQuery();
-
-            //encerrando a conexao
-            conexao.Close();
-
-            //se a quantidade de linhas que ele afetou for maior que 0, retorna true - se conseguiu cadastrar
-            if (linhasAfetadas > 0)
+            try
             {
-                return true;
+                //conecta no banco de dados
+                MySqlConnection conexao = ConexaoDB.CriarConexao();
+
+                //oq ele vai executar do sql
+                string sql = "INSERT INTO tbusuarios (nome, usuario, senha) VALUES (@nome, @usuario, @senha);";
+
+                //abri a conexao com o banco de dados
+                conexao.Open();
+
+                //executa o comando sql
+                MySqlCommand comando = new MySqlCommand(sql, conexao);
+
+                //troca o valor dos @ pelas informações que serão cadastradas 
+                //essas informações vieram das funções
+                comando.Parameters.AddWithValue("@nome", nome);
+                comando.Parameters.AddWithValue("@usuario", usuario);
+                comando.Parameters.AddWithValue("@senha", senha);
+
+                //executando no banco de dados
+                int linhasAfetadas = comando.ExecuteNonQuery();
+
+                //encerrando a conexao
+                conexao.Close();
+
+                //se a quantidade de linhas que ele afetou for maior que 0, retorna true - se conseguiu cadastrar
+                if (linhasAfetadas > 0)
+                {
+                    return true;
+                }
+
+                //se não conseguiu, retorna false (< 0 linhas)
+                else
+                {
+                    return false;
+                }
             }
-
-            //se não conseguiu, retorna false (< 0 linhas)
-            else
+            catch (Exception erro)
             {
+                //aparece quando da erro, a segunda aspas eh o titulo, o buttons cria um botão e o icon cria um icone
+                MessageBox.Show($"Erro ao cadastrar: {erro.Message}", "ERRO", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
                 return false;
             }
+
         }
     }
 }
