850e2ba3b9ac1fc2263765723d08c9d23f4f85e9
eduarda virgilio <eduarda.virgilio2630@gmail.com>
2024-11-19 11:32:16 -0300

    att banco de dados

--- EstruturaDB/banco-de-dados.sql
+++ EstruturaDB/banco-de-dados.sql
@@ -2,17 +2,35 @@ create database dbagenda;
 
 use dbagenda;
 
-select * from tbusuarios;
-
 create table tbusuarios(
 nome VARCHAR(80) NOT NULL,
 usuario VARCHAR(20) NOT NULL primary key,
 senha VARCHAR(20) NOT NULL);
 
+
 create table tbcategorias(
 	categorias VARCHAR(40) NOT NULL,
-	cod_categoria INT AUTO_INCREMENT primary key
+	cod_categoria INT AUTO_INCREMENT primary key,
+    usuario varchar (20) 
     );
+
+delimiter $$
+create trigger trinsertcategoria
+	before
+	insert
+	on tbcategoria
+	for each row
+begin 
+	set new.usuario = CURRENT_USER();
+end;
+
+$$
+
+delimiter ;
+
+select * from tbusuarios;
+
+select * from mySql.user;
 
 select cod_categoria AS 'Código', categorias AS 'Categoria'
 from tbcategorias;
