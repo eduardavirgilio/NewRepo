90863187fdc453a8b1d26a78732972bc363f4e24
Eduarda Virgilio <eduarda.virgilio2630@gmail.com>
2024-11-25 17:22:02 -0300

    att o banco de dados

--- EstruturaDB/banco-de-dados.sql
+++ EstruturaDB/banco-de-dados.sql
@@ -22,11 +22,108 @@ create trigger trinsertcategoria
 	for each row
 begin 
 	set new.usuario = CURRENT_USER();
+end;tbusuarios
+
+$$
+
+delimiter ;
+
+delimiter $$
+create trigger trlogcategoriadelete
+	after
+	delete
+	on tbcategorias
+	for each row
+begin 
+	insert into tblog
+		(usuario,
+        ultima_alteracao,
+        descricao)
+        
+	values
+    (USER(),
+    CURRENT_TIMESTAMP(),
+    CONCAT('A categoria ', OLD.categorias, ' foi deletada'));
+    
 end;
 
 $$
 
 delimiter ;
+
+delimiter $$
+create trigger trinsertcategoria
+	before
+	insert
+	on tbcategoria
+	for each row
+begin 
+	set new.usuario = CURRENT_USER();
+end;tbusuarios
+
+$$
+
+delimiter ;
+
+-- trigger de update
+delimiter $$
+create trigger trlogcategoriauupdate
+	after
+	update
+	on tbcategorias
+	for each row
+begin 
+	insert into tblog
+		(usuario,
+        ultima_alteracao,
+        descricao)
+        
+	values
+    (USER(),
+    CURRENT_TIMESTAMP(),
+    CONCAT('A categoria ', OLD.categorias, ' foi alterada'));
+    
+end;
+
+$$
+
+delimiter ;
+
+-- tabela do log
+
+create table tblog(
+	cod_log INT AUTO_INCREMENT primary key,
+	usuario VARCHAR(80) NOT NULL,
+    ultima_alteracao DATETIME,
+    descricao VARCHAR(300) NOT NULL
+    );
+
+-- trigger para inserir categorias
+delimiter $$
+create trigger trlogcategoriainsert
+	after
+	insert
+	on tbcategorias
+	for each row
+begin 
+	insert into tblog
+		(usuario,
+        ultima_alteracao,
+        descricao)
+        
+	values
+    (USER(),
+    CURRENT_TIMESTAMP(),
+    CONCAT('A categoria ', NEW.categorias, ' foi inserida'));
+    
+end;
+
+$$
+
+delimiter ;
+
+
+select * from tblog;
 
 select * from tbusuarios;
 
