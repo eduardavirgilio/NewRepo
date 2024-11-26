9d110be9b46b9fb46cb0fcd91a98ab8a29ccf366
Eduarda Virgilio <eduarda.virgilio2630@gmail.com>
2024-11-26 16:39:35 -0300

    so coloquei o get

--- Controller/ContatoController.cs
+++ Controller/ContatoController.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using ProjetoAgenda.Data;
+using ProjetoAgenda.VariableGlobal;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +12,47 @@ namespace ProjetoAgenda.Controller
 {
     internal class ContatoController
     {
+        public DataTable GetContatos()
+        {
+            //criando uma conexao vazia
+            MySqlConnection conexao = null;
+            try
+            {
+                //criando uma conexao com a conexao db que ja estava criada
+                conexao = ConexaoDB.CriarConexao(UserSession.usuario, UserSession.senha);
 
-    }
+                //montando o select que retorna todas as categorias
+                string sql = @"select cod_categoria, usuario, categorias from tbcategorias where usuario = User();";
+
+                //abri a conexao
+                conexao.Open();
+
+                //criei um adaptador
+                MySqlDataAdapter adaptador = new MySqlDataAdapter(sql, conexao);
+
+                //MySqlCommand comando = new MySqlCommand(sql, conexao);
+
+
+                //comando.Parameters.AddWithValue("@usuario", $"{UserSession.usuario}@%");
+
+                //tabela em branco
+                DataTable tabela = new DataTable();
+
+                //pedindo para o adaptador preencher a tabela
+                adaptador.Fill(tabela);
+
+                //retorno a tabela preenchida
+                return tabela;
+            }
+            catch (Exception erro)
+            {
+                MessageBox.Show($"ERRO AO RECUPERAR CATEGORIAS: {erro.Message}");
+                return new DataTable();
+            }
+
+            finally
+            {
+                conexao.Close();
+            }
+        }
 }
